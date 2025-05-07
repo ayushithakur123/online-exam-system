ea5b0af3f563a7acf5ae5a2236a85fae3c15a5f2
ayushithakur123 <127535310+ayushithakur123@users.noreply.github.com>
2025-05-07 13:37:14 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,6 +1,6 @@
 <?php
 //all the variables defined here are accessible in all the files that include this one
-$con= new mysqli('ec2-13-233-103-194.ap-south-1.compute.amazonaws.com','admin','admin123456789','oesm')or die("Could not connect to mysql".mysqli_error($con));
+$con= new mysqli('http://13.233.103.194/','admin','admin123456789','oesm')or die("Could not connect to mysql".mysqli_error($con));
 
 
 ?>
